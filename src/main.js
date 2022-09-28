8632b4246f42b15ef56376848f73b57c832a2d8c
abeauvois <abeauvois@gmail.com>
2022-09-28 10:48:23 +0200

    use yarn

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import animateTitle from './features/animateTitle'
 import createBadge from './features/createBasge'
 import './styles/style.css'
 
-console.log('Welcome to Vite + JS + Webflow!')
+console.log('Welcome to Creapods')
 
 createBadge()
 animateTitle()
